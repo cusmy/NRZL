48f00f58d9392476869a84fab0fc71a569c9e50f
cusmy <55727972+cusmy@users.noreply.github.com>
2020-06-30 20:32:35 +0700

    Update NRZ-L.py

--- NRZ-L.py
+++ NRZ-L.py
@@ -1,8 +1,6 @@
-cuk = "HELLO WOLRD"
-  
-# print("String :" + (cuk)) 
+string = input("Masukan String : ")
 
-res = ''.join(format(ord(i), 'b') for i in cuk) 
+res = ''.join(format(ord(i), 'b') for i in string) 
 er = (res[0:4])
 et = (res[7])
 ey = (res [11:14])
@@ -12,5 +10,5 @@ ep = (res [56:60])
 ew = (res [61:65])
 eq = (res [-11 : -7])
 T = er + et + ey + eu + eo + ep + ew + eq
-print(len(res))
 print("Conversi Ke NRZ-L: " +(T))
+
