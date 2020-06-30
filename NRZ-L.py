e0065b8625890401700bf4c7ad3fe84f2901e054
cusmy <55727972+cusmy@users.noreply.github.com>
2020-06-30 22:00:53 +0700

    Update NRZ-L.py

--- NRZ-L.py
+++ NRZ-L.py
@@ -1,14 +1,28 @@
 string = input("Masukan String : ")
 
 res = ''.join(format(ord(i), 'b') for i in string) 
-er = (res[0:4])
-et = (res[7])
-ey = (res [11:14])
-eu = (res [22:26])
-eo = (res [38:42])
-ep = (res [56:60])
-ew = (res [61:65])
-eq = (res [-11 : -7])
-T = er + et + ey + eu + eo + ep + ew + eq
-print("Conversi Ke NRZ-L: " +(T))
+ar= [None]*len(res)
+dump = 0
+dump_a = []
 
+for i in range(len(res)):
+   if len(res[i:i+4]) == 4:
+      ar[i] = res[i:i+4]
+   else:
+      del ar[i:-1]
+      del ar[-1]
+      break
+for i in ar:
+    bah = int (i,2)
+    
+
+
+    if bah > dump or bah == dump :
+        dump_a.append(1) 
+    else:
+        dump_a.append(0)
+
+    dump = bah
+    
+s = ''.join(map(str, dump_a))
+print (s)
